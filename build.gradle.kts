658bddbcffdb584346af287903c0d838bf0a6621
SangHyunGil <zizon5941@gmail.com>
2025-07-27 22:56:08 +0900

    feat: initailze

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    val kotlinVersion = "2.2.0"
+    id("idea")
+    id("java")
+    kotlin("jvm") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
+}
+
+group = "com.order"
+version = "0.0.1-SNAPSHOT"
+
+// TODO. Gradle Convention
+// https://docs.gradle.org/current/samples/sample_convention_plugins.html
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "kotlin")
+
+    repositories {
+        mavenCentral()
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+    tasks.compileKotlin {
+        compilerOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = JvmTarget.fromTarget("21")
+        }
+    }
+
+    configurations {
+        compileOnly {
+            extendsFrom(configurations.annotationProcessor.get())
+        }
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+}
