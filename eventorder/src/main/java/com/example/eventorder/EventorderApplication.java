005770fd36da3ebd37459779db1ff9f8fa33afac
0takkk <syt0719@naver.com>
2025-07-27 22:03:40 +0900

    init: setting project

---
+++ eventorder/src/main/java/com/example/eventorder/EventorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventorderApplication.class, args);
+    }
+
+}
